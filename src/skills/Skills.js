c466f7085ae6098006e724e9645af7c23a58ea40
Krystle Mitchell <mitchell.krystle2@gmail.com>
2023-07-27 19:15:03 -0700

    Completes Skills
    Skills component and CSS is complete

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Avatar from '../avatar/Avatar'
-import SkillsMenu from './SkillsMenu.js'
+import SkillsMenu from "./SkillsMenu"
 
 export default class Skills extends Component {
   render() {
     return (
       <>
+      SkillsMenu
       <Avatar page="skills" />
       <SkillsMenu />
       
---
+++ src/skills/skillsData.js
@@ -0,0 +1,54 @@
+const skills = {
+        1: [
+            {
+            title: "HTML",
+            level: 6,
+        },
+        {
+            title: "CSS",
+            level: 6,
+        },
+        {
+            title: "JavaScript",
+            level: 6,
+        },
+        {
+            title: "TypeScript",
+            level: 3,
+        },
+        {
+            title: "React",
+            level: 6,
+        },
+
+    ],
+
+        2: [{
+            title: "PostgreSql",
+            level: 6,
+        },
+        {
+            title: "Node.js",
+            level: 6,
+        },
+        {
+            title: "MongeDB",
+            level: 6,
+        },
+        {
+            title: "Git",
+            level: 3,
+        },
+        {
+            title: "SQL",
+            level: 6,
+        },
+        
+        {
+            title: "Express.js",
+            level: 6,
+        },
+    ],
+};
+
+export default skills;
