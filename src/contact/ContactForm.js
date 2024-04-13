f5fbfabe000b229777ae54b45e6ab7f138c435ff
Krystle <krystle@Krystles-MBP.lan>
2024-04-12 18:00:47 -0700

    Added Success Pop-up with Confetti

--- src/contact/ContactForm.js
+++ src/contact/ContactForm.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 import DOMPurify from 'dompurify';
+import Confetti from 'react-dom-confetti';
 import '../styles/contactForm.css'; // Import your CSS file
 
 export default function ContactForm() {
   const initialState = {
-    name: "",
-    email: "",
-    message: "",
+    name: '',
+    email: '',
+    message: '',
   };
 
   const [formData, setFormData] = useState(initialState);
   const [errors, setErrors] = useState({});
   const [isLoading, setIsLoading] = useState(false);
-  const [isSent, setIsSent] = useState(false);
+  const [isMessageSent, setIsMessageSent] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -45,13 +46,13 @@ export default function ContactForm() {
     emailjs
       .send(serviceId, templateId, sanitizedData, userId)
       .then((response) => {
-        console.log("Email sent successfully", response.text);
+        console.log('Email sent successfully', response.text);
         setFormData(initialState);
         setErrors({});
-        setIsSent(true);
+        setIsMessageSent(true);
       })
       .catch((error) => {
-        console.error("Error sending email", error);
+        console.error('Error sending email', error);
       })
       .finally(() => {
         setIsLoading(false);
@@ -60,62 +61,82 @@ export default function ContactForm() {
 
   const validateForm = () => {
     const { name, email, message } = formData;
-    // Implement your validation logic here
     const errors = {};
     if (!name) {
-      errors.name = "Name is required";
+      errors.name = 'Name is required';
     }
     if (!email) {
-      errors.email = "Email is required";
+      errors.email = 'Email is required';
     } else if (!isValidEmail(email)) {
-      errors.email = "Invalid email format";
+      errors.email = 'Invalid email format';
     }
     if (!message) {
-      errors.message = "Message is required";
+      errors.message = 'Message is required';
     }
     return errors;
   };
 
   const isValidEmail = (email) => {
-    // Simple email format validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
+  const confettiConfig = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 40,
+    elementCount: 70,
+    dragFriction: 0.12,
+    duration: 3000,
+    stagger: 3,
+    width: '10px',
+    height: '10px',
+    colors: ['#ff0000', '#00ff00', '#0000ff'],
+  };
+
   return (
     <div className="contact-menu"> {/* Center the form */}
-      <form onSubmit={handleSubmit} className="form-container">
-        <input
-          id="name"
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          placeholder="Your Name"
-        />
-        {errors.name && <div className="error">{errors.name}</div>}
-        <input
-          id= "email"
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Your Email"
-        />
-        {errors.email && <div className="error">{errors.email}</div>}
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          placeholder="Your Message"
-        ></textarea>
-        {errors.message && <div className="error">{errors.message}</div>}
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? "Sending..." : "Send Message"}
-        </button>
-        {isSent && <div className="success">Message sent successfully!</div>}
-      </form>
+      {!isMessageSent && (
+        <form onSubmit={handleSubmit} className="form-container">
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Your Name"
+          />
+          {errors.name && <div className="error">{errors.name}</div>}
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Your Email"
+          />
+          {errors.email && <div className="error">{errors.email}</div>}
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="Your Message"
+          ></textarea>
+          {errors.message && <div className="error">{errors.message}</div>}
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      )}
+      {isMessageSent && (
+        <div className="success-message">
+          <Confetti active={isMessageSent} config={confettiConfig} />
+          <p>SUCCESS!!!</p>
+          <p>Message sent successfully!</p>
+          <p>You can exit this page</p>
+        </div>
+      )}
     </div>
   );
 }
