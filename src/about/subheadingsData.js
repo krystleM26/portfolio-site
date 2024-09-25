132285abe212c3c07d8d5ceba878d250946f8471
Krystle <krystle@Krystles-MBP.lan>
2024-09-24 20:12:24 -0700

    Updated About info

--- src/about/subheadingsData.js
+++ src/about/subheadingsData.js
@@ -43,9 +43,11 @@ const subheadingsData = {
             title: "Web Developer",
             content: (
                 <>
-                <p>
-                    Build and deploy responsive websites.
-                </p>
+                <ul>
+                   <li>Developed and managed a user-friendly website to improve employee access and streamline daily reporting processes.</li>
+                   <li>Implemented responsive design to ensure optimal performance across devices, enhancing user experience.</li>
+                   <li></li>
+                </ul>
              
                 </>
             ),
@@ -54,28 +56,21 @@ const subheadingsData = {
             title: "Office Manager",
             content: (
                 <>
-                <p>
-                  Help staff with technical issues.                </p>
-                  <p>
-                  Input scheudules and payroll              </p>
-                  <p>
-                  Maintain wordpress website.             </p>
+               <ul>
+                <li> Led document management, ensuring accuracy and compliance with internal procedures and safety standards.
+                </li>
 
+                <li> Developed and maintained document templates, streamlining onboarding processes and appointment scheduling.</li>
+                 <li>Managed the digital filing structure using Google Workspace, ensuring proper classification and organization of documents.</li>
+                <li>Utilized analytics tools to monitor site performance and user engagement, leading to continuous improvements.</li> 
+                <li>Conducted regular maintenance and updates to ensure security and functionality of the website</li>
+                <li>Trained staff on website usage and features, facilitating smoother operations and increased adoption.</li>      
+                </ul>
              
                 </>
             ),
         },
-        {
-            title: "Admin Assistant",
-            content: (
-                <>
-                <p>
-                    Helped digitize filing and scheduling systems.
-                </p>
-             
-                </>
-            ),
-        },
+        
     ],
 }
 
