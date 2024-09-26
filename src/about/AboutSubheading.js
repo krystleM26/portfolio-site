43fc4cb9054915c7c5dfe1125caf204a6276805a
Krystle <krystle@Krystles-MBP.lan>
2024-09-26 14:25:51 -0700

    Updated about me

--- src/about/AboutSubheading.js
+++ src/about/AboutSubheading.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import classNames from 'classnames'
 import '../styles/aboutMenu.css'
 import PropTypes from 'prop-types';
@@ -9,7 +9,7 @@ const AboutSubheading = ({ title, content, active, onClick, menuItem }) => {
 
   AboutSubheading.propTypes = {
     title: PropTypes.string.isRequired,
-    content: PropTypes.element.isRequired, // Assuming content is a React element
+    content: PropTypes.node.isRequired, // 'node allows str'
     active: PropTypes.bool.isRequired,
     onClick: PropTypes.func.isRequired,
     menuItem: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
@@ -17,11 +17,38 @@ const AboutSubheading = ({ title, content, active, onClick, menuItem }) => {
 
   
   const subContainerClass = `sub-container-${menuItem}`
-    
+  const [typedContent, setTypedContent] = useState('');
+  const [isTyping, setIsTyping] = useState(false)
+
+  useEffect(() => {
+    if(active && !isTyping && typeof content === 'string') {
+      setIsTyping(true);
+      let index = 0;
+      const speed = 100;
+
+      const type = () => {
+        if(index < content.length) {
+          setTypedContent(prev => prev + content.charAt(index));
+          index++;
+          setTimeout(type, speed)
+        } else {
+          setIsTyping(false)
+        }
+      }
+      setTypedContent('');
+      type();
+    } else if (!active) {
+      setTypedContent('');
+      setIsTyping(false);
+    }
+  }, [active, content, isTyping])
+
     return (
       <div className={classNames(subContainerClass, { "active-subheading": active })} >
       <h3 onClick={onClick}>{title}</h3>
-        <div className="p-container">{content}</div>
+        <div className="p-container">{active && typeof content === 'string' 
+        ? <p>{typedContent}</p> : active ? content : null} 
+        </div>
       </div>
     )
   
--- src/about/subheadingsData.js
+++ src/about/subheadingsData.js
@@ -2,16 +2,22 @@ import React from "react"
 
 const subheadingsData = {
     1:[
-        {
+        { 
             title: "About Me",
-            content: (
-                <>
-                <p>
-                    Full Stack Developer with a passion for creating and developing web applications.
-                </p>
-             
-                </>
-            ),
+            content: `
+            Hello! I’m Krystle Mitchell, a writer turned web developer.
+            As a Full-Stack Developer, I blend creativity and technical expertise to build engaging and functional web applications.
+            My journey from writing to coding has equipped me with a unique perspective on storytelling through technology,
+            allowing me to create user-centric designs that not only meet business objectives but also resonate with users.
+            I specialize in  JavaScript, React, Node.js, etc, and I thrive in collaborative environments where I can contribute to 
+            all aspects of development, from initial concept to deployment. When I’m not coding, you can find me reading, swimming, skiiing, 
+            or hiking. I’am passionate about continuous learning and staying up-to-date with industry trends, which drives my commitment to 
+            delivering innovative solutions. Let’s connect and explore how we can work together to bring your ideas to life!
+            
+            `
+                
+                        
+        ,
         },
     ],
     2:[
@@ -19,9 +25,9 @@ const subheadingsData = {
             title: "FullStack Developer",
             content: (
                 <>
-                <p>
-                  BloomTech - Full Stack Developer
-                </p>
+                    <p>
+                         BloomTech - Full Stack Developer
+                    </p>
              
                 </>
             ),
@@ -30,9 +36,9 @@ const subheadingsData = {
             title: "Journalisn",
             content: (
                 <>
-                <p>
-                    AA Journalism
-                </p>
+                    <p>
+                         AA Journalism
+                    </p>
              
                 </>
             ),
@@ -43,11 +49,11 @@ const subheadingsData = {
             title: "Web Developer",
             content: (
                 <>
-                <ul>
-                   <li>Developed and managed a user-friendly website to improve employee access and streamline daily reporting processes.</li>
-                   <li>Implemented responsive design to ensure optimal performance across devices, enhancing user experience.</li>
-                   <li></li>
-                </ul>
+                    <ul>
+                        <li>Developed and managed a user-friendly website to improve employee access and streamline daily reporting processes.</li>
+                        <li>Implemented responsive design to ensure optimal performance across devices, enhancing user experience.</li>
+                        <li></li>
+                    </ul>
              
                 </>
             ),
@@ -56,16 +62,14 @@ const subheadingsData = {
             title: "Office Manager",
             content: (
                 <>
-               <ul>
-                <li> Led document management, ensuring accuracy and compliance with internal procedures and safety standards.
-                </li>
-
-                <li> Developed and maintained document templates, streamlining onboarding processes and appointment scheduling.</li>
-                 <li>Managed the digital filing structure using Google Workspace, ensuring proper classification and organization of documents.</li>
-                <li>Utilized analytics tools to monitor site performance and user engagement, leading to continuous improvements.</li> 
-                <li>Conducted regular maintenance and updates to ensure security and functionality of the website</li>
-                <li>Trained staff on website usage and features, facilitating smoother operations and increased adoption.</li>      
-                </ul>
+                    <ul>
+                        <li> Led document management, ensuring accuracy and compliance with internal procedures and safety standards.</li>
+                        <li> Developed and maintained document templates, streamlining onboarding processes and appointment scheduling.</li>
+                        <li>Managed the digital filing structure using Google Workspace, ensuring proper classification and organization of documents.</li>
+                        <li>Utilized analytics tools to monitor site performance and user engagement, leading to continuous improvements.</li> 
+                        <li>Conducted regular maintenance and updates to ensure security and functionality of the website</li>
+                        <li>Trained staff on website usage and features, facilitating smoother operations and increased adoption.</li>      
+                    </ul>
              
                 </>
             ),
