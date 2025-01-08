1c57b0b71ef44ac77fe5589cea3933755aacc8f7
Krystle <krystle@Krystles-MBP.lan>
2025-01-07 19:56:18 -0800

    Added Medium Posts to Website

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../styles/blog.css';
 
 
 
 
 const Blog = () => {
-  return (
-    <div>
-      <h1>Coming Soon</h1>
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    </div>
-  );
+  useEffect(() => {
+    fetchMediumPosts = async () => {
+      try {
+        const response = await fetch(
+          `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/green-code`
+        )
+        const data = await response.json();
+        setPosts(data.items);
+      } catch (error) {
+        console.error('Error fetching Medium posts:', error);
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchMediumPosts();
+
+  }, [])
+
+  return (
+    <div className='blog-container'>
+        <h1>My Blog</h1>
+        {
+          loading ? (
+            <p>Loading Posts...</p>
+
+          ) : posts.length > 0 ? (
+            <div className='posts-list'>
+              {posts.map((post, index) => (
+                <div key = {index} className='post'>
+                  <a href={post.link} target='_blank' rel="noopener noreferrer">
+                    <h2>{post.title}</h2>
+                  </a>
+                  <p>{post.pubDate}</p>
+                  <p>{post.description}</p>
+                </div>
+              ))}
+        </div>
+          ): (
+            <p>No posts found</p>
+          )}
+      </div>
+  )
+};
 
   
-}
+  
 
 export default Blog
